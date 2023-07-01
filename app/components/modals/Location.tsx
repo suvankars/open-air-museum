6e67742c4aad23fed538d9de21f0179b8c9c8cce
suvankar <suvankar.17@gmail.com>
2023-07-01 18:00:10 +0200

    Added map to the location

--- app/components/modals/Location.tsx
+++ app/components/modals/Location.tsx
@@ -1,19 +1,28 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import { FC } from "react"
 
 import CountrySelect from "../Input/countrySelect"
 import { CountryValue } from "@/app/type"
+import dynamic from "next/dynamic"
+import { useMemo } from "react"
 
 interface LocationProps {
     value: CountryValue,
     onChangeCountry: (location: CountryValue) => void
 }
-const Location: FC<LocationProps> = ({value, onChangeCountry})=>{
-    
+const Location: FC<LocationProps> = ({ value, onChangeCountry }) => {
+
+    const Map = dynamic(() => import('../Map'), {
+        loading: () => <p>Loading...</p>,
+        ssr: false,
+      })
+
     return (
         <div>
             <h3>Please let user know where your property located</h3>
             <p> Select your country </p>
-            <CountrySelect value={value} onChangeCountry={onChangeCountry}/>
+            <CountrySelect value={value} onChangeCountry={onChangeCountry} />
+            <Map center={value?.latlong} />
         </div>
     )
 }
